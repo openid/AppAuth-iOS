eb95e3736eac6ae43affbb6db7298e4862636cd4
Zach Margolis <zachary.margolis@gsa.gov>
2017-07-19 11:20:35 -0400

    Add end_session_endpoint

--- UnitTests/OIDServiceDiscoveryTests.m
+++ UnitTests/OIDServiceDiscoveryTests.m
@@ -34,6 +34,7 @@ static NSString *const kTokenEndpointKey = @"token_endpoint";
 static NSString *const kUserinfoEndpointKey = @"userinfo_endpoint";
 static NSString *const kJWKSURLKey = @"jwks_uri";
 static NSString *const kRegistrationEndpointKey = @"registration_endpoint";
+static NSString *const kEndSessionEndpointKey = @"end_session_endpoint";
 static NSString *const kScopesSupportedKey = @"scopes_supported";
 static NSString *const kResponseTypesSupportedKey = @"response_types_supported";
 static NSString *const kResponseModesSupportedKey = @"response_modes_supported";
@@ -97,6 +98,7 @@ static NSString *const kOPTosURIKey = @"op_tos_uri";
     kUserinfoEndpointKey : @"User Info Endpoint",
     kJWKSURLKey : @"http://www.example.com/jwks",
     kRegistrationEndpointKey : @"Registration Endpoint",
+    kEndSessionEndpointKey : @"https://www.example.com/logout",
     kScopesSupportedKey : @"Scopes Supported",
     kResponseTypesSupportedKey : @"Response Types Supported",
     kResponseModesSupportedKey : @"Response Modes Supported",
@@ -466,6 +468,7 @@ TestURLFieldBackedBy(tokenEndpoint, kTokenEndpointKey, kTestURL);
 TestURLFieldBackedBy(userinfoEndpoint, kUserinfoEndpointKey, kTestURL);
 TestURLFieldBackedBy(jwksURL, kJWKSURLKey, kTestURL);
 TestURLFieldBackedBy(registrationEndpoint, kRegistrationEndpointKey, kTestURL);
+TestURLFieldBackedBy(endSessionEndpoint, kEndSessionEndpointKey, kTestURL);
 TestFieldBackedBy(scopesSupported, kScopesSupportedKey, @"Scopes Supported");
 TestFieldBackedBy(responseTypesSupported, kResponseTypesSupportedKey, @"Response Types Supported");
 TestFieldBackedBy(responseModesSupported, kResponseModesSupportedKey, @"Response Modes Supported");
