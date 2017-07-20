69933bd17c3dc7f90bc73f05662a2dde5d55b22f
Zach Margolis <zachary.margolis@gsa.gov>
2017-07-20 16:00:58 -0400

    Add OIDLogoutRequest

---
+++ UnitTests/OIDLogoutRequestTests.h
@@ -0,0 +1,13 @@
+#import <XCTest/XCTest.h>
+
+@class OIDLogoutRequest;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OIDLogoutRequestTests : XCTestCase
+
++ (OIDLogoutRequest *)testInstance;
+
+@end
+
+NS_ASSUME_NONNULL_END
