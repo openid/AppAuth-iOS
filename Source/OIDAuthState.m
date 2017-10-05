879b26e2791db70a4ecc516eed74226ba397e183
Jason Job <jason.allan.job@gmail.com>
2017-10-05 14:58:09 -0700

    Pass authorizationRequest additionalParameters to tokenExchangeRequest
    A small change so that any additionalParameters on the authorizationRequest would now also get set on the subsequent tokenExchangeRequest. It seems that this is the way AppAuth-Android already functions.

--- Source/OIDAuthState.m
+++ Source/OIDAuthState.m
@@ -119,7 +119,7 @@ static const NSUInteger kExpiryTimeTolerance = 60;
                                // code is intended for this client, and performs the authorization
                                // code exchange
                                OIDTokenRequest *tokenExchangeRequest =
-                                   [authorizationResponse tokenExchangeRequest];
+                                   [authorizationResponse tokenExchangeRequestWithAdditionalParameters:authorizationRequest.additionalParameters];
                                [OIDAuthorizationService
                                    performTokenRequest:tokenExchangeRequest
                                               callback:^(OIDTokenResponse *_Nullable tokenResponse,
