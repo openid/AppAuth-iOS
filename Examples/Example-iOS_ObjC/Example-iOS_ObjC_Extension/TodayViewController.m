ff5b4d98f1cdf6e7038def70f9d8bec1a39390bc
Julien Bodet <julien.bodet92@gmail.com>
2018-03-11 16:48:45 -0400

    Multiple fixes to the app extension example

--- Examples/Example-iOS_ObjC/Example-iOS_ObjC_Extension/TodayViewController.m
+++ Examples/Example-iOS_ObjC/Example-iOS_ObjC_Extension/TodayViewController.m
@@ -1,10 +1,20 @@
-//
-//  TodayViewController.m
-//  Example-iOS_ObjC_Extension
-//
-//  Created by Julien Bodet on 2018-03-11.
-//  Copyright © 2018 William Denniss. All rights reserved.
-//
+/*! @file TodayViewController.m
+ @brief AppAuth iOS SDK Example
+ @copyright
+ Copyright 2015 Google Inc. All Rights Reserved.
+ @copydetails
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "TodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
@@ -31,10 +41,6 @@ static NSString *const kAppAuthExampleAuthStateKey = @"authState";
     }
     
     [self loadState];
-}
-
-- (void) viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
 }
 
 - (void)widgetActiveDisplayModeDidChange:(NCWidgetDisplayMode)activeDisplayMode
@@ -207,6 +213,7 @@ static NSString *const kAppAuthExampleAuthStateKey = @"authState";
                          dateString,
                          log];
 }
+
 - (IBAction)clearLogTextView:(UIButton *)sender {
     _logTextView.text = @"";
 }
