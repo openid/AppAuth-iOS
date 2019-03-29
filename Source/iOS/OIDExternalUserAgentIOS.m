f87521f3663da3909966eaa9d3d27f9d5633342c
Cassiano Monteiro <cmonteiro@dialpad.com>
2019-03-29 14:27:19 -0400

    Cherrypick of "Fixed else condition for mobile Safari. Renamed variable to openedUserAgent."

--- Source/iOS/OIDExternalUserAgentIOS.m
+++ Source/iOS/OIDExternalUserAgentIOS.m
@@ -85,7 +85,7 @@ static id<OIDSafariViewControllerFactory> __nullable gSafariViewControllerFactor
 
   _externalUserAgentFlowInProgress = YES;
   _session = session;
-  BOOL openedSafari = NO;
+  BOOL openedUserAgent = NO;
   NSURL *requestURL = [request externalUserAgentRequestURL];
 
   // iOS 12 and later, use ASWebAuthenticationSession
@@ -115,13 +115,13 @@ static id<OIDSafariViewControllerFactory> __nullable gSafariViewControllerFactor
         }
       }];
       _webAuthenticationVC = authenticationVC;
-      openedSafari = [authenticationVC start];
+      openedUserAgent = [authenticationVC start];
     }
   }
   // iOS 11, use SFAuthenticationSession
   // SFAuthenticationSession doesn't work with guided access (rdar://40809553)
   if (@available(iOS 11.0, *)) {
-    if (!openedSafari && !UIAccessibilityIsGuidedAccessEnabled()) {
+    if (!openedUserAgent && !UIAccessibilityIsGuidedAccessEnabled()) {
       __weak OIDExternalUserAgentIOS *weakSelf = self;
       NSString *redirectScheme = request.redirectScheme;
       SFAuthenticationSession *authenticationVC =
@@ -145,32 +145,33 @@ static id<OIDSafariViewControllerFactory> __nullable gSafariViewControllerFactor
         }
       }];
       _authenticationVC = authenticationVC;
-      openedSafari = [authenticationVC start];
+      openedUserAgent = [authenticationVC start];
     }
   }
   // iOS 9 and 10, use SFSafariViewController
   if (@available(iOS 9.0, *)) {
-    if (!openedSafari) {
+    if (!openedUserAgent) {
       SFSafariViewController *safariVC =
           [[SFSafariViewController alloc] initWithURL:requestURL];
       safariVC.delegate = self;
       _safariVC = safariVC;
       [_presentingViewController presentViewController:safariVC animated:YES completion:nil];
-      openedSafari = YES;
+      openedUserAgent = YES;
     }
+  }
   // iOS 8 and earlier, use mobile Safari
-  } else {
-    openedSafari = [[UIApplication sharedApplication] openURL:requestURL];
+  if (!openedUserAgent){
+    openedUserAgent = [[UIApplication sharedApplication] openURL:requestURL];
   }
 
-  if (!openedSafari) {
+  if (!openedUserAgent) {
     [self cleanUp];
     NSError *safariError = [OIDErrorUtilities errorWithCode:OIDErrorCodeSafariOpenError
                                             underlyingError:nil
                                                 description:@"Unable to open Safari."];
     [session failExternalUserAgentFlowWithError:safariError];
   }
-  return openedSafari;
+  return openedUserAgent;
 }
 
 - (void)dismissExternalUserAgentAnimated:(BOOL)animated completion:(void (^)(void))completion {
