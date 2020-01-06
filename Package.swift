bd1a88b423654df257318e134b67fd3b6f9d93d5
Balazs Szamody <fr3qfly@gmail.com>
2020-01-07 10:01:31 +1100

    Fixed the Library name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AppAuth-iOS",
-            targets: ["AppAuth-iOS"]),
+            targets: ["AppAuth"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "AppAuth-iOS",
+            name: "AppAuth",
             dependencies: [],
             path: "Source",
             cSettings: [
