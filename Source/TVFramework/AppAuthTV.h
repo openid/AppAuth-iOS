37a2d7892995ecb2ddad4de53b4aa6a328884e16
Souleiman Benhida <souleben@gmail.com>
2020-07-06 12:08:48 -0400

    Add AppAuthTV Framework to xcodeproj, separate from AppAuth_tvOS which is based off full AppAuth

---
+++ Source/TVFramework/AppAuthTV.h
@@ -0,0 +1,47 @@
+//
+//  AppAuthTV.h
+//  AppAuthTV
+//
+//  Created by Souleiman Benhida on 7/6/20.
+//  Copyright © 2020 OpenID Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppAuthTV.
+FOUNDATION_EXPORT double AppAuthTVVersionNumber;
+
+//! Project version string for AppAuthTV.
+FOUNDATION_EXPORT const unsigned char AppAuthTVVersionString[];
+
+#import <AppAuthTV/OIDAuthState.h>
+#import <AppAuthTV/OIDAuthStateChangeDelegate.h>
+#import <AppAuthTV/OIDAuthStateErrorDelegate.h>
+#import <AppAuthTV/OIDAuthorizationRequest.h>
+#import <AppAuthTV/OIDAuthorizationResponse.h>
+#import <AppAuthTV/OIDAuthorizationService.h>
+#import <AppAuthTV/OIDError.h>
+#import <AppAuthTV/OIDErrorUtilities.h>
+#import <AppAuthTV/OIDExternalUserAgent.h>
+#import <AppAuthTV/OIDExternalUserAgentRequest.h>
+#import <AppAuthTV/OIDExternalUserAgentSession.h>
+#import <AppAuthTV/OIDGrantTypes.h>
+#import <AppAuthTV/OIDIDToken.h>
+#import <AppAuthTV/OIDRegistrationRequest.h>
+#import <AppAuthTV/OIDRegistrationResponse.h>
+#import <AppAuthTV/OIDResponseTypes.h>
+#import <AppAuthTV/OIDScopes.h>
+#import <AppAuthTV/OIDScopeUtilities.h>
+#import <AppAuthTV/OIDServiceConfiguration.h>
+#import <AppAuthTV/OIDServiceDiscovery.h>
+#import <AppAuthTV/OIDTokenRequest.h>
+#import <AppAuthTV/OIDTokenResponse.h>
+#import <AppAuthTV/OIDTokenUtilities.h>
+#import <AppAuthTV/OIDURLSessionProvider.h>
+#import <AppAuthTV/OIDEndSessionRequest.h>
+#import <AppAuthTV/OIDEndSessionResponse.h>
+
+#import <AppAuthTV/OIDTVAuthorizationResponse.h>
+#import <AppAuthTV/OIDTVAuthorizationRequest.h>
+#import <AppAuthTV/OIDTVAuthorizationService.h>
+#import <AppAuthTV/OIDTVServiceConfiguration.h>
